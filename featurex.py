c2a4222e21b840673f3cf70a1fa4e3aa9845026b
Samuel Arleo <saar1312@yahoo.com>
2016-12-27 22:21:22 +0000

    Adding image and descriptor classes

---
+++ featurex.py
@@ -0,0 +1,12 @@
+class image:
+	def __init__(self, img=None, keyp=None, desc=None, hist=None):
+		self.img = img
+		self.descriptors = desc
+		self.keypoint = keyp
+		self.histogram = hist
+
+class descriptor:
+	def __init__(self, vector=None, label=None)
+		self.vector = vector
+		self. label = label
+
