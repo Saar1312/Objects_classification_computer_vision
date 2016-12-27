c854b4f75695b7d21ffc73f0e8d34b9e3e83573d
Samuel Arleo <saar1312@yahoo.com>
2016-12-27 22:23:19 +0000

    Fixing error in class definition

--- featurex.py
+++ featurex.py
@@ -6,7 +6,7 @@ class image:
 		self.histogram = hist
 
 class descriptor:
-	def __init__(self, vector=None, label=None)
+	def __init__(self, vector=None, label=None):
 		self.vector = vector
 		self. label = label
 
